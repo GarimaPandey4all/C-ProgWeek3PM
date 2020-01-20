10083f072274859655325d4d3ea1af10fe795d6c
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2020-01-20 03:53:39 -0800

    Add files via upload

---
+++ FORMATSP.C
@@ -0,0 +1,18 @@
+/*  Wap to perform format specifiers
+	Name: Garima Pandey
+	Date: 20/01/2019  */
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float area=20.000000f;
+	clrscr();
+
+	printf("%.2f", area);
+
+	getch();
+	return 0;
+
+}
